42fa579b4452a7ca19bc15c0e7ac46a7bad0cfa5
MisaelGurgel <misael_shyon@hotmail.com>
2019-08-22 17:40:31 -0300

    Add cliente2

---
+++ cliente2/cliente.py
@@ -0,0 +1,17 @@
+import socket
+HOST = '10.25.2.17' #Falta definir o ip do servidor     10.25.2.21 ip da maquina
+PORT = 5565 # porta do server
+
+
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_socket.connect((HOST, PORT))
+while True:
+    msg = input('Digite a mensagem: ')
+
+    msg = msg.encode('utf-8')
+
+    tcp_socket.send(msg)
+
+    if not msg: break
+
+tcp_socket.close()
---
+++ cliente2/cliente2.py
@@ -0,0 +1,24 @@
+import socket
+HOST = '10.25.2.17' #Falta definir o ip do servidor     10.25.2.21 ip da maquina
+PORT = 5565 # porta do server
+
+
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_socket.connect((HOST, PORT))
+nomeArquivo = input("Nome do Arquivo")
+fd = open (nomeArquivo, "r")
+nomeArquivo = nomeArquivo.encode('utf-8')
+tcp_socket.send(nomeArquivo)
+while True:
+    
+    msg = input('Digite a mensagem: ')
+
+    msg = msg.encode('utf-8')
+
+    tcp_socket.send(msg)
+
+    if not msg: break
+
+tcp_socket.close()
+
+
